7dfb17830c301ed5748867336a704b29b21b315d
sandro-pasquali <spasquali@gmail.com>
2015-03-18 16:24:02 -0500

    ++

--- push.js
+++ push.js
@@ -23,7 +23,7 @@ var constructor = function() {
 	this.buildAndTest = function(path) {
 		console.log(path);
 		exec('(cd ' + path + '; npm i; gulp; npm test)', function(err) {
-			console.log("BUILDTEST", err);
+			console.log("BUILDTEST", arguments);
 		}); 
 	}
 
