bb5ba14d47ac3b8341cb0a6c8219fadbadd168c1
sandro-pasquali <spasquali@gmail.com>
2015-03-18 16:41:48 -0500

    ++

--- push.js
+++ push.js
@@ -20,22 +20,20 @@ var constructor = function() {
 		});
 	};
 	
-	this.buildAndTest = function(path) {
+	this.buildAndTest = function(path, cb) {
 		console.log(path);
 		exec('(cd ' + path + '; npm i; gulp; npm test)', function(err) {
-			console.log("BUILDTEST", arguments);
+			cb(err);
 		}); 
 	}
 
-	this.killProcess = function(name, cb) {
+	this.killAndRestart = function(name) {
 		pm2.connect(function() {
 			pm2.describe(pm2Name, function(err, list) {
 				list.forEach(function(obj) {
 					exec('pm2 delete ' + obj.pm_id, function() {
 						console.log(obj.pm_id + ' - deleted');
 						fs.unlinkSync('swanson.log');
-						
-						cb && cb();
 					});
 				});
 			});
@@ -50,49 +48,16 @@ var constructor = function() {
 				this.killProcess(pm2Name);
 			}
 			
-			this.buildAndTest(clonePath);
+			this.buildAndTest(clonePath, function(err) {
+				if(err) {
+					throw new Error("New repo not production ready!");
+				}
+				
+				this.killAndRestart(pm2Name);
+			});
 			
 		}.bind(this));
-		
-		//this.killProcess(pm2Name);
-		
 	};
 };
 
 module.exports = new constructor();
-
-/*
-var clone = require("./").Clone.clone;
-
-// Clone a given repository into a specific folder.
-clone("https://github.com/nodegit/nodegit", "tmp", null)
-  // Look up this known commit.
-  .then(function(repo) {
-    // Use a known commit sha from this repository.
-    return repo.getCommit("59b20b8d5c6ff8d09518454d4dd8b7b30f095ab5");
-  })
-  // Look up a specific file within that commit.
-  .then(function(commit) {
-    return commit.getEntry("README.md");
-  })
-  // Get the blob contents from the file.
-  .then(function(entry) {
-    // Patch the blob to contain a reference to the entry.
-    return entry.getBlob().then(function(blob) {
-      blob.entry = entry;
-      return blob;
-    });
-  })
-  // Display information about the blob.
-  .then(function(blob) {
-    // Show the name, sha, and filesize in byes.
-    console.log(blob.entry.name() + blob.entry.sha() + blob.size() + "b");
-
-    // Show a spacer.
-    console.log(Array(72).join("=") + "\n\n");
-
-    // Show the entire file.
-    console.log(String(blob));
-  })
-  .catch(function(err) { console.log(err); });
-*/
