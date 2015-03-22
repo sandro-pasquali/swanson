428152ba21dc8ae23bea7020c402aeb8b2a9d296
sandro-pasquali <spasquali@gmail.com>
2015-03-22 12:30:18 -0500

    ++

--- push.js
+++ push.js
@@ -21,7 +21,7 @@ var args = process.argv.slice(2);
 var sourceDir = path.dirname(args[2]);
 var buildDir = args[0];
 
-var command = 'git clone ' + args[1] + ' ' + buildDir + ';cd ' + buildDir + ';npm i; gulp;npm test; rm -rf ' + sourceDir + '; mv ' + buildDir + ' ' + sourceDir + ';pm2 restart ' + args[2];
+var command = 'git clone ' + args[1] + ' ' + buildDir + ';cd ' + buildDir + ';npm i; gulp;npm test; rm -rf ' + sourceDir + '/build; mv ' + buildDir + '/build ' + sourceDir + ';pm2 restart ' + args[2];
 
 exec(command, function() {
 	log.info('restart complete');
